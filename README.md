042870312347ba16a8e452baa559b756cdb38b22
MARCELO VIEIRA DA SILVA <25132416@CFP127ALUNO.LOCAL>
2025-09-16 10:50:28 -0300

    projeto api cep

---
+++ README.md
@@ -0,0 +1,14 @@
+# Consumo da API-CEP com JavaScript
+
+![](./img/preview.PNG)
+
+Página de cadastro de endereço, onde o usuario pode digitar apenas o CEP e o restante do endereço é preenchido automaticamente utilizando a API-CEP.
+
+## Tecnologias
+* HTML
+* CSS
+* JAVASCRIPT
+* API-CEP
+
+## Autor
+[Marcelo Vieira](<https://www.linkedin.com/in/marcelovieirasilva/>)
---
+++ main.js
@@ -0,0 +1,28 @@
+/* *****************************************************************************************
+* Objetivo: Preencher automaticamente o formulario de acordo com o CEP digitado e validações (API CEP)
+* Data: 16/09/2025
+* Autor: Marcelo Vieira
+* Versão: 1.0
+* *****************************************************************************************/
+
+'use strict'
+
+async function pesquisarCep(cep){
+    const url = `https://cdn.apicep.com/file/apicep/${cep}.json`
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+}
+
+async function preencherCampos ({target}){
+    const infoCep = await pesquisarCep(target.value)
+    document.getElementById('endereco').value = infoCep.address
+    document.getElementById('bairro').value = infoCep.district
+    document.getElementById('cidade').value = infoCep.city
+    document.getElementById('estado').value = infoCep.state
+
+}
+
+document.getElementById('cep')
+        .addEventListener('focusout', preencherCampos)
+        
